78388f34a6bec39c3b49276d525279551e4bfc0e
Kisuke Saito <kisuke_saito@be-wo-od.com>
2022-10-17 02:46:33 +0900

    fix

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -2,11 +2,12 @@ import { useRouter } from 'next/router'
 import Nav from '../components/Nav'
 
 const IdPage = () => {
-  const { asPath } = useRouter()
+  const { asPath, route } = useRouter()
   return (
     <>
       <Nav />
       <p>Hello, I'm the {asPath} page</p>
+      <p>Hello, I'm the {route} page, actually</p>
     </>
   )
 }
